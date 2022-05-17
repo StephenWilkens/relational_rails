10017831b659087b87e70429aeb712359cf430eb
Stephen Wilkens <stephenwilkens@gmail.com>
2022-05-17 15:22:08 -0600

    Add edit and update actions to companies controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -16,6 +16,16 @@ class CompaniesController < ApplicationController
     redirect_to "/companies/#{new_comp.id}"
   end
 
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    company = Company.find(params[:id])
+    company.update(company_params)
+    redirect_to "/companies/#{company.id}"
+  end
+
   private
   def company_params
     params.permit(:name, :location, :total_stock, :pro_deal, :created_at, :updated_at, :company_id)
