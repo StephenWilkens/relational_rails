a746503c7b03e90d3e46549612b2ba683cf5d00e
Stephen Wilkens <stephenwilkens@gmail.com>
2022-05-17 21:20:37 -0600

    Add update link to bikes show page, add edit page, add edit test, add edit and update actions, and add update route and view

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -6,4 +6,19 @@ class BikesController < ApplicationController
   def show
     @bike = Bike.find(params[:id])
   end
+
+  def edit
+    @bike = Bike.find(params[:id])
+  end
+
+  def update
+    bike = Bike.find(params[:id])
+    bike.update(bike_params)
+    redirect_to("/bikes/#{bike.id}")
+  end
+
+  private
+  def bike_params
+    params.permit(:name, :front_travel, :full_suspension, :created_at, :updated_at)
+  end
 end
